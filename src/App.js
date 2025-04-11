0e163aaa27398ea22ba63299501b6d679cdf95cb
kirillbrostar <detkov2002@bk.ru>
2025-04-11 23:50:24 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import React from 'react';
 import './index.scss';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = React.useState(0) // указываем начальное значение 0
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button onClick={()=> setCount(count+ 1)} className="minus">- Минус</button> 
+        <button onClick={()=> setCount(count-1)} className="plus">Плюс +</button>
+        <button onClick={()=> setCount(0)} className="minus">Сброс</button>
       </div>
     </div>
   );
